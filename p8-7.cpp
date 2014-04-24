8ce0668542d15f75671e82c516595c7f9e0ca6b9
Zhu Li <zhuli19901106@gmail.com>
2014-04-25 00:58:26 +0800

    add careercup files
    201404250058

--- p8-7.cpp
+++ p8-7.cpp
@@ -3,7 +3,7 @@
 //	1. Peer-to-peer chatting, which goes through the chatting server as relay.
 //	2. Group chatting and broadcasting, which is distributed by the server.
 //	3. It will be social, so people can make friends, which brings in graph theory.
-//	4. If keeps track of the chatting messages sent by the user, will need massive storage to hold them.
+//	4. If it keeps track of the chatting messages sent by the user, it will need massive storage to hold them.
 //	5. Oh my god, what kind of companies would ask such complicated problem... I'm just a new grad, spare me.
 int main()
 {
